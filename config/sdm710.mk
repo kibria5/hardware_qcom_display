ff46b6ef237b3bb2193b4ad644bc088c774c637c
Naseer Ahmed <naseer@codeaurora.org>
2019-11-28 18:39:44 -0500

    display: Disable gralloc module
    This module is no longer supported. IMapper and IAllocator
    APIs must be used instead
    
    Change-Id: I0c4c459c6c4acf06f9ae2c4257f8da857bdfb628

--- config/sdm710.mk
+++ config/sdm710.mk
@@ -8,7 +8,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     modetest \
-    gralloc.sdm710 \
     hwcomposer.sdm710 \
     libqdutils \
     libqdMetaData
