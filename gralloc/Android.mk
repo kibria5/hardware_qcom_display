1972b9ecd671d6b712eb2d00bef969db77754672
EndCredits <endcredits@crepuscular-aosp.icu>
2024-11-19 12:39:42 +0800

    Convert libgrallocutils and gpu_tonemapper to blueprint
     * Support ELF check for some blobs
    
    Change-Id: I11dec853d8cfaddb01ba96a2e76be38fe2d52073
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -20,35 +20,6 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := gr_device_impl.cpp
 include $(BUILD_SHARED_LIBRARY)
 
-#libgrallocutils
-include $(CLEAR_VARS)
-LOCAL_MODULE                  := libgrallocutils
-LOCAL_VENDOR_MODULE           := true
-LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl  \
-                                  android.hardware.graphics.mapper@2.0 \
-                                  android.hardware.graphics.mapper@2.1
-LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion
-
-ifeq ($(TARGET_USES_ALIGNED_YCRCB_HEIGHT),true)
-    LOCAL_CFLAGS              += -DUSE_ALIGNED_YCRCB_HEIGHT
-endif
-ifeq ($(TARGET_USES_ALIGNED_YCBCR_HEIGHT),true)
-    LOCAL_CFLAGS              += -DUSE_ALIGNED_YCBCR_HEIGHT
-endif
-ifeq ($(TARGET_USES_UNALIGNED_NV21_ZSL),true)
-    LOCAL_CFLAGS              += -DUSE_UNALIGNED_NV21_ZSL
-endif
-ifeq ($(TARGET_USES_UNALIGNED_YCRCB),true)
-    LOCAL_CFLAGS              += -DUSE_UNALIGNED_YCRCB
-endif
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
-LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
-include $(BUILD_SHARED_LIBRARY)
-
 #libgralloccore
 include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgralloccore
